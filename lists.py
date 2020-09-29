4ae8f608ed0aaa85027caceaf626e54b113229e6
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-29 11:07:59 -0400

    inserted line

--- lists.py
+++ lists.py
@@ -1,6 +1,15 @@
 def main():
     file = open("silly.txt",'r')
-    
+    all_of_file = file.readlines()
+    print(all_of_file)
 
 
+def reverser():
+    rev_file= open("reversible.txt", 'r')
+    all_lines = rev_file.readlines()
+    print(all_lines)
+    to_insert = all_lines [0]
+    print(f"welcome to {to_insert}, fantastic student")
+
+reverser()
 main()
