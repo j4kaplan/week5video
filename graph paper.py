78b45a682d1ff6dbc423313e497f79aa4b6632ad
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-29 12:14:33 -0400

    vertical lines on graph

---
+++ graph paper.py
@@ -0,0 +1,16 @@
+import arcade
+def draw_lines ():
+    arcade.open_window(800,800,"lines demo")
+    arcade.set_background_color(arcade.color.APPLE_GREEN)
+    width= arcade.get_window().width
+    height= arcade.get_window().height
+    arcade.start_render()
+    #drawing goes here
+    for location in range (80,width, 80):
+        arcade.draw_line(location,0,location, height, arcade.color.BLACK_OLIVE, 2)
+    arcade.finish_render()
+
+    arcade.run()
+
+
+draw_lines()
