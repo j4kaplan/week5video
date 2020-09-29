bcd879e5f71e315cc8111130526d8d9b2cb27eaa
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-29 12:32:30 -0400

    formatted numbers

--- lists.py
+++ lists.py
@@ -1,16 +1,27 @@
+import random
+
 def main():
     file = open("silly.txt",'r')
     all_of_file = file.readlines()
-    for line in all_of_file:
-        print(line)
+    random_line= random.choice(all_of_file)
+    print(random_line)
 
 
 def reverser():
     rev_file= open("reversible.txt", 'r')
     all_lines = rev_file.readlines()
     print(all_lines)
-    to_insert = all_lines [0]
+    to_insert = all_lines[0]
     print(f"welcome to {to_insert}, fantastic student")
 
-reverser()
-main()
+#reverser()
+#main()
+
+def string_formatting_demo():
+    result= 100/3
+    formatted_number = "{:5.3f}".format(result)
+    print(formatted_number)
+
+
+string_formatting_demo()
+
